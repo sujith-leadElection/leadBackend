5ab6f6ce920dece9d48bacacd69bfd80645ba91c
Senthil-Riddhish <82893678+Senthil-Riddhish@users.noreply.github.com>
2024-12-27 11:55:44 +0530

    push

--- Database/connection.js
+++ Database/connection.js
@@ -2,7 +2,7 @@
 import mongoose from "mongoose";
 require("dotenv").config();
 export default async () => {
-  return await mongoose.connect(process.env.MONGO_URI, {
+  return await mongoose.connect("mongodb+srv://Sujith:Sv02RDzCvIA6ctqX@cluster0.g3igd2e.mongodb.net/LeadDB?retryWrites=true&w=majority", {
     serverSelectionTimeoutMS: 20000, // Increase timeout to 20 seconds
   });  
 };
